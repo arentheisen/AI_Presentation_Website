87f569a05544f1bac7dc76933fbc5c7b5e2012d8
arentheisen <arentheisen@gmail.com>
2025-03-11 12:48:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const sectionId = this.getAttribute('href').substring(1);
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
